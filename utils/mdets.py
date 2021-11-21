324d8b37b1ecc8bc71c08b7053ee124f0073f6f7
Starlysh <74881934+YuuKomoe@users.noreply.github.com>
2021-11-21 16:40:32 +0100

    Added extra imports for later

--- utils/mdets.py
+++ utils/mdets.py
@@ -1,12 +1,13 @@
 from utils.csetconf import auto_start, check_updates, threads, retries, mail_access, hide_passwords, print_bad, save_bad, proxy_protocol, proxy_dupe, proxy_bad, debugging, dev_mode
 from utils.colors import yellow, red, green, cyan, blue, white, magenta
+
 from colorama import Fore, init, Style
+import requests, ctypes, time, os, sys, keyboard
 
 class Details():
     global credits, announces, mark, autostart_config
 
-    autostart_config = (f'''
-{red}> {white}Auto start: {yellow}{auto_start}
+    autostart_config = (f'''{red}> {white}Auto start: {yellow}{auto_start}
 {red}> {white}Check for updates: {yellow}{check_updates}
 {red}> {white}Threads: {yellow}{threads}
 {red}> {white}Retries: {yellow}{threads}
@@ -18,8 +19,7 @@ class Details():
 {red}> {white}Proxy duplications: {yellow}{proxy_dupe}
 {red}> {white}Proxy bad: {yellow}{proxy_bad}
 {red}> {white}Debugging: {yellow}{debugging}
-{red}> {white}Dev mode: {yellow}{dev_mode}
-    ''')
+{red}> {white}Dev mode: {yellow}{dev_mode}''')    
 
     credits = (f'''
         Credits to Tails Development Team,
